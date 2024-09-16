cb4c9d89e2dfc7f0ce98681009bec5e52e5e6912
vagrant <vagrant@pw-aws-box>
2024-09-16 09:46:11 +0000

    modify outputs.tf

---
+++ project_2/terraform/outputs.tf
@@ -0,0 +1,24 @@
+output "public_subnet1_id" {
+  value = aws_subnet.public_subnet1.id
+}
+
+output "public_subnet2_id" {
+  value = aws_subnet.public_subnet2.id
+}
+
+output "private_subnet1_id" {
+  value = aws_subnet.private_subnet1.id
+}
+
+output "private_subnet2_id" {
+  value = aws_subnet.private_subnet2.id
+}
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+output "aws_lb" {
+  description = "DNS_name"
+  value       = aws_lb.alb.dns_name
+
+}
