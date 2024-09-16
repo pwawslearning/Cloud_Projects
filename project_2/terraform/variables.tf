9d2e96fe6d5df067b6bec9f8969cbc028c54a03f
vagrant <vagrant@pw-aws-box>
2024-09-16 09:41:40 +0000

    add main.tf version.tf outputs.tf variables.tf

---
+++ project_2/terraform/variables.tf
@@ -0,0 +1,77 @@
+variable "profile" {
+  type = string
+
+}
+
+variable "vpc-cidr" {
+  type = string
+}
+
+variable "public_subnet1" {
+
+}
+
+variable "project-name" {
+
+}
+
+variable "public_subnet2" {
+
+}
+
+variable "private_subnet1" {
+
+}
+
+variable "private_subnet2" {
+
+}
+
+variable "public_sub1a_name" {
+
+}
+
+variable "public_sub2b_name" {
+
+}
+variable "private_subnet1_name" {
+
+}
+variable "private_subnet2_name" {
+}
+variable "vpc_id" {
+
+}
+variable "image_id" {
+
+}
+variable "instance_type" {
+
+}
+variable "path" {
+
+}
+variable "desired_capacity" {
+
+}
+variable "max_size" {
+
+}
+variable "min_size" {
+
+}
+variable "bucket_name" {
+
+}
+variable "status" {
+
+}
+variable "source_path" {
+
+}
+variable "role_name" {
+
+}
+variable "policy_arn" {
+  
+}
