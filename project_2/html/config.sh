e161360f1a53a098a2f5199692860a8cac5d35dc
pwawslearning <pwawslearning@gmail.com>
2024-09-19 11:00:35 +0000

    update config.sh

--- project_2/html/config.sh
+++ project_2/html/config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Update package index
-sudo apt install update -y
+sudo -su ubuntu
 sudo apt-get update
 
 # Install Apache web server
-sudo apt install -y apache2
+sudo apt install apache2 -y
 
 # Start Apache service
 sudo systemctl start apache2
@@ -15,9 +15,8 @@ sudo systemctl enable apache2
 #Restart Apache service
 sudo systemctl restart apache2
 
-sudo apt install awscli -y
-
 cd /var/www/html
+sudo apt install awscli -y
 sudo mkdir image
 sudo aws s3 cp s3://apache-websvr-bk/Apache.png image/
 sudo rm index.html
